6eabaa977e7394a8a84eefd5a421712e6bf14eb4
Kevin Ngo <me@ngokevin.com>
2016-07-17 13:15:23 -0700

    initial work in progress

--- index.js
+++ index.js
@@ -1,51 +1,96 @@
 /* global AFRAME */
+var anime = require('animejs');
 
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
 
+var utils = AFRAME.utils;
+var getComponentProperty = utils.entity.getComponentProperty;
+var setComponentProperty = utils.entity.setComponentProperty;
+var styleParser = utils.styleParser.parse;
+
 /**
  * Animation component for A-Frame.
  */
 AFRAME.registerComponent('animation', {
-  schema: { },
+  schema: {
+    delay: {default: 0},
+    direction: {default: ''},
+    duration: {default: 1000},
+    easing: {default: 'easeOutElastic'},
+    loop: {default: false},
+    property: {default: ''},
+    to: {default: ''}
+  },
 
-  /**
-   * Set if component needs multiple instancing.
-   */
-  multiple: false,
+  multiple: true,
 
-  /**
-   * Called once when component is attached. Generally for initial setup.
-   */
-  init: function () { },
+  init: function () {
+    this.animation = null;
+    this.isPlaying = false;
+  },
 
-  /**
-   * Called when component is attached and when component data changes.
-   * Generally modifies the entity based on the data.
-   */
-  update: function (oldData) { },
+  update: function () {
+    var data = this.data;
+    var el = this.el;
 
-  /**
-   * Called when a component is removed (e.g., via removeAttribute).
-   * Generally undoes all modifications to the entity.
-   */
-  remove: function () { },
+    var value = AFRAME.utils.entity.getComponentProperty(el, data.property);
 
-  /**
-   * Called on each scene tick.
-   */
-  // tick: function (t) { },
+    if (value.constructor === String) {
+      var target = {};
+      target[data.property] = value;
+      value = target;
+    }
 
-  /**
-   * Called when entity pauses.
-   * Use to stop or remove any dynamic or background behavior such as events.
-   */
-  pause: function () { },
+    var config = {
+      targets: [value],
+      autoplay: false,
+      direction: data.direction,
+      duration: data.duration,
+      loop: data.loop,
+      update: function () {
+        AFRAME.utils.entity.setComponentProperty(el, data.property, value[data.property]);
+      }
+    };
+    config[data.property] = data.to;
 
-  /**
-   * Called when entity resumes.
-   * Use to continue or add any dynamic or background behavior such as events.
-   */
-  play: function () { }
+    this.stopAnimation();
+    this.animation = anime(config);
+  },
+
+  remove: function () {
+    this.stopAnimation();
+  },
+
+  tick: function (t) {
+    if (!this.animation) { return; }
+    this.animation.tick(t);
+  },
+
+  pause: function () {
+    this.stopAnimation();
+  },
+
+  play: function () {
+    if (!this.animation) { return; }
+    this.animation.play();
+  },
+
+  stopAnimation: function () {
+    if (!this.animation) { return; }
+    this.animation.pause();
+  }
 });
+
+function getPropertyType (el, property) {
+  var split = property.split('.');
+  var componentName = split[0];
+  var propertyName = split[1];
+  var schema = (el.components[componentName] || AFRAME.components[componentName]).schema;
+
+  if (propertyName) {
+    return schema[propertyName].type;
+  }
+  return schema.type;
+}
